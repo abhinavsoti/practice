ed1bbedb44f3d4764533942178028e0e69fb1f18
Abhinav Soti <soti.abhinav@gmail.com>
2020-08-12 20:34:05 +0530

    Implemented getting user name, checking all the system processes currently in running state

--- pythonStatments_practice.py
+++ pythonStatments_practice.py
@@ -1,7 +1,22 @@
-print('execution starts now')
-mystring = 'Hello World'
-print (mystring)
-print('Hello World')
+import datetime
+import getpass
+import psutil
+import time
 
-for letter in mystring:
-    print(letter)
+username = getpass.getuser()
+print('execution starts now at ' + str(datetime.datetime.now()) + ' by ' + username)
+
+print ('Following process are running at the moment at ' + str(datetime.datetime.now()))
+
+i=0
+
+while i < 20:
+    process_list = [p.name() for p in psutil.process_iter()]
+    for running_process in process_list:
+        print(running_process)
+    print ('Going to sleep for 3 seconds')
+    time.sleep(3)
+    i = i+1
+    print (i)
+
+print ('execution completed at ' + str(datetime.datetime.now()) + ' by ' + username)
