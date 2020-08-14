b355a291f9d78177fdbc2b364b56d196f4bbb6c9
Abhinav Soti <soti.abhinav@gmail.com>
2020-08-14 22:53:22 +0530

    list comprehension examples code and ennumerate code

---
+++ listComprehension.py
@@ -0,0 +1,32 @@
+######################################################################################
+# Script Name: listComprehension                                                     #
+# Description: This script will print the user executing the program and will also   #
+#              keep checking the current system processes running at the moment.     #
+# Version    : 1.0                                                                   #
+######################################################################################
+
+## Old method of creating list out of a string ##
+mylist = 'Hello'
+newlist = []
+
+for letter in mylist:
+    newlist.append(letter)
+
+print(newlist)
+
+## List comprehension in python to replicate the above behaviour ##
+
+mystring = 'Hello'
+
+mylist_n = []
+
+mylist_n = [letter for letter in mystring]
+
+print (mylist_n)
+
+## enumerate example
+
+mystr = 'Hello World'
+
+for index,item in enumerate(mystr):
+    print(index, item)
